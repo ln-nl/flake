b2f6629e4f69368057e6f92688500453ab5d24fe
ln-nl <123864217+ln-nl@users.noreply.github.com>
2023-05-06 10:37:03 +0300

    nit: Formatting

--- flake.nix
+++ flake.nix
@@ -3,81 +3,177 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config = {
-          allowUnfree = true;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config = {
+        allowUnfree = true;
       };
-    in
-    rec {
+    };
+  in rec {
+    packages.x86_64-linux = {
+      audiosprite = import ./audiosprite {
+        inherit (pkgs) lib buildNpmPackage fetchFromGitHub;
+      };
 
-      packages.x86_64-linux = {
-        audiosprite = import ./audiosprite {
-          inherit (pkgs) lib buildNpmPackage fetchFromGitHub;
-        };
+      fbx2gltf = import ./fbx2gltf {
+        inherit (pkgs) fetchurl stdenv lib;
+      };
 
-        fbx2gltf = import ./fbx2gltf {
-          inherit (pkgs) fetchurl stdenv lib;
-        };
+      http-server = import ./http-server {
+        inherit (pkgs) lib buildNpmPackage fetchFromGitHub;
+      };
 
-        http-server = import ./http-server {
-          inherit (pkgs) lib buildNpmPackage fetchFromGitHub;
-        };
-
-        flashback = (
+      flashback =
+        (
           import ./flashback/Cargo.nix {
             inherit pkgs;
           }
-        ).rootCrate.build;
+        )
+        .rootCrate
+        .build;
 
-        flashplayer-standalone-debugger = import ./flashplayer/standalone.nix {
-          inherit (pkgs) stdenv lib fetchurl alsaLib atk bzip2 cairo curl
-            expat fontconfig freetype gdk-pixbuf glib glibc graphite2 gtk2
-            harfbuzz libdrm libffi libglvnd libpng libvdpau nspr nss pango
-            pcre pixman zlib unzip;
+      flashplayer-standalone-debugger = import ./flashplayer/standalone.nix {
+        inherit
+          (pkgs)
+          stdenv
+          lib
+          fetchurl
+          alsaLib
+          atk
+          bzip2
+          cairo
+          curl
+          expat
+          fontconfig
+          freetype
+          gdk-pixbuf
+          glib
+          glibc
+          graphite2
+          gtk2
+          harfbuzz
+          libdrm
+          libffi
+          libglvnd
+          libpng
+          libvdpau
+          nspr
+          nss
+          pango
+          pcre
+          pixman
+          zlib
+          unzip
+          ;
 
-          inherit (pkgs.xorg) libICE libSM libX11 libXau libXcomposite
-            libXcursor libXrender libXt libXxf86vm libxcb libxshmfence
-            libXdamage libXdmcp libXext libXfixes libXi libXinerama libXrandr;
-        };
-
-        libreswan = import ./libreswan {
-          inherit (pkgs) lib stdenv fetchurl nixosTests pkg-config systemd gmp
-            unbound bison flex pam libevent libcap_ng libxcrypt curl nspr bash runtimeShell iproute2
-            iptables procps coreutils gnused gawk nss which python3 libselinux
-            ldns xmlto docbook_xml_dtd_412 docbook_xsl findXMLCatalogs;
-        };
-
-        networkmanager-l2tp = import ./networkmanager-l2tp {
-          inherit (pkgs) lib stdenv substituteAll fetchFromGitHub
-            autoreconfHook libtool intltool pkg-config file findutils gtk3
-            networkmanager ppp xl2tpd libsecret libnma glib;
-
-          inherit (packages.x86_64-linux) libreswan;
-        };
-
-        nodePackages = import ./node-packages {
-          inherit (pkgs) system stdenv lib;
-          inherit pkgs;
-        };
-
-        texturepacker = import ./texturepacker {
-          inherit (pkgs) stdenv dpkg fetchurl autoPatchelfHook;
-          inherit (pkgs.qt6) qtwayland;
-        };
-
-        videoshot = import ./videoshot {
-          inherit (pkgs) wf-recorder slurp libnotify coreutils bash resholve;
-        };
-
-        yo = import ./yo {
-          inherit (pkgs) lib buildNpmPackage fetchFromGitHub;
-        };
-
+        inherit
+          (pkgs.xorg)
+          libICE
+          libSM
+          libX11
+          libXau
+          libXcomposite
+          libXcursor
+          libXrender
+          libXt
+          libXxf86vm
+          libxcb
+          libxshmfence
+          libXdamage
+          libXdmcp
+          libXext
+          libXfixes
+          libXi
+          libXinerama
+          libXrandr
+          ;
       };
 
+      libreswan = import ./libreswan {
+        inherit
+          (pkgs)
+          lib
+          stdenv
+          fetchurl
+          nixosTests
+          pkg-config
+          systemd
+          gmp
+          unbound
+          bison
+          flex
+          pam
+          libevent
+          libcap_ng
+          libxcrypt
+          curl
+          nspr
+          bash
+          runtimeShell
+          iproute2
+          iptables
+          procps
+          coreutils
+          gnused
+          gawk
+          nss
+          which
+          python3
+          libselinux
+          ldns
+          xmlto
+          docbook_xml_dtd_412
+          docbook_xsl
+          findXMLCatalogs
+          ;
+      };
+
+      networkmanager-l2tp = import ./networkmanager-l2tp {
+        inherit
+          (pkgs)
+          lib
+          stdenv
+          substituteAll
+          fetchFromGitHub
+          autoreconfHook
+          libtool
+          intltool
+          pkg-config
+          file
+          findutils
+          gtk3
+          networkmanager
+          ppp
+          xl2tpd
+          libsecret
+          libnma
+          glib
+          ;
+
+        inherit (packages.x86_64-linux) libreswan;
+      };
+
+      nodePackages = import ./node-packages {
+        inherit (pkgs) system stdenv lib;
+        inherit pkgs;
+      };
+
+      texturepacker = import ./texturepacker {
+        inherit (pkgs) stdenv dpkg fetchurl autoPatchelfHook;
+        inherit (pkgs.qt6) qtwayland;
+      };
+
+      videoshot = import ./videoshot {
+        inherit (pkgs) wf-recorder slurp libnotify coreutils bash resholve;
+      };
+
+      yo = import ./yo {
+        inherit (pkgs) lib buildNpmPackage fetchFromGitHub;
+      };
     };
+  };
 }
