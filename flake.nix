9cdb7fe90fbfa8b89d6e3c949fdbb01f8fafa5a3
ln-nl <123864217+ln-nl@users.noreply.github.com>
2024-04-05 11:00:43 +0300

    chore: Formatting

--- flake.nix
+++ flake.nix
@@ -3,99 +3,82 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      config = {
-        allowUnfree = true;
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+    rec {
+      packages.x86_64-linux = {
+        audiosprite = import ./audiosprite { inherit (pkgs) lib buildNpmPackage fetchFromGitHub; };
+
+        npm-audit-html = import ./npm-audit-html { inherit (pkgs) lib buildNpmPackage fetchFromGitHub; };
+
+        fbx2gltf = import ./fbx2gltf { inherit (pkgs) fetchurl stdenv lib; };
+
+        http-server = import ./http-server { inherit (pkgs) lib buildNpmPackage fetchFromGitHub; };
+
+        flashback = (import ./flashback/Cargo.nix { inherit pkgs; }).rootCrate.build;
+
+        flashplayer-standalone-debugger = import ./flashplayer/standalone.nix {
+          inherit (pkgs)
+            stdenv
+            lib
+            fetchurl
+            alsaLib
+            atk
+            bzip2
+            cairo
+            curl
+            expat
+            fontconfig
+            freetype
+            gdk-pixbuf
+            glib
+            glibc
+            graphite2
+            gtk2
+            harfbuzz
+            libdrm
+            libffi
+            libglvnd
+            libpng
+            libvdpau
+            nspr
+            nss
+            pango
+            pcre
+            pixman
+            zlib
+            unzip
+            ;
+
+          inherit (pkgs.xorg)
+            libICE
+            libSM
+            libX11
+            libXau
+            libXcomposite
+            libXcursor
+            libXrender
+            libXt
+            libXxf86vm
+            libxcb
+            libxshmfence
+            libXdamage
+            libXdmcp
+            libXext
+            libXfixes
+            libXi
+            libXinerama
+            libXrandr
+            ;
+        };
       };
     };
-  in rec {
-    packages.x86_64-linux = {
-      audiosprite = import ./audiosprite {
-        inherit (pkgs) lib buildNpmPackage fetchFromGitHub;
-      };
-
-      npm-audit-html = import ./npm-audit-html {
-        inherit (pkgs) lib buildNpmPackage fetchFromGitHub;
-      };
-
-      fbx2gltf = import ./fbx2gltf {
-        inherit (pkgs) fetchurl stdenv lib;
-      };
-
-      http-server = import ./http-server {
-        inherit (pkgs) lib buildNpmPackage fetchFromGitHub;
-      };
-
-      flashback =
-        (
-          import ./flashback/Cargo.nix {
-            inherit pkgs;
-          }
-        )
-        .rootCrate
-        .build;
-
-      flashplayer-standalone-debugger = import ./flashplayer/standalone.nix {
-        inherit
-          (pkgs)
-          stdenv
-          lib
-          fetchurl
-          alsaLib
-          atk
-          bzip2
-          cairo
-          curl
-          expat
-          fontconfig
-          freetype
-          gdk-pixbuf
-          glib
-          glibc
-          graphite2
-          gtk2
-          harfbuzz
-          libdrm
-          libffi
-          libglvnd
-          libpng
-          libvdpau
-          nspr
-          nss
-          pango
-          pcre
-          pixman
-          zlib
-          unzip
-          ;
-
-        inherit
-          (pkgs.xorg)
-          libICE
-          libSM
-          libX11
-          libXau
-          libXcomposite
-          libXcursor
-          libXrender
-          libXt
-          libXxf86vm
-          libxcb
-          libxshmfence
-          libXdamage
-          libXdmcp
-          libXext
-          libXfixes
-          libXi
-          libXinerama
-          libXrandr
-          ;
-      };
-    };
-  };
 }
